a9db608ea8669a352c5cfd5aa36bf8f2a6fe8ea9
telosaarwyn <153449704+telosaarwyn@users.noreply.github.com>
2024-01-10 21:50:19 +0800

    Driver

---
+++ main/Driver.java
@@ -0,0 +1,18 @@
+package main;
+
+import frames.MainFrame;
+
+/**
+ * The Driver class is the entry point for the burger vending machine program.
+ * 
+ * @author Lim, Theoni Anne
+ *         Telosa, Arwyn Gabrielle
+ * @version 2.0
+ * @since 07-18-23 
+ */
+public class Driver {
+
+    public static void main(String[] args) {
+        new MainFrame().setVisible(true);
+    }
+}
